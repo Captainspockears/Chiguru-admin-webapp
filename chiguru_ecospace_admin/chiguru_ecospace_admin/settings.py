dcd7b21534dcf111ca7d8cbe5eb59ec1b17211d1
captainspockears <ruthuparna1998@gmail.com>
2020-06-11 23:35:44 +0530

    Added cloudsql

--- chiguru_ecospace_admin/chiguru_ecospace_admin/settings.py
+++ chiguru_ecospace_admin/chiguru_ecospace_admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,12 +74,43 @@ WSGI_APPLICATION = 'chiguru_ecospace_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# Install PyMySQL as mysqlclient/MySQLdb to use Django's mysqlclient adapter
+# See https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-db-api-drivers
+# for more information
+pymysql.version_info = (1, 4, 6, 'final', 0)  # change mysqlclient version
+pymysql.install_as_MySQLdb()
+
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/chiguru-ecospace-website:asia-south1:polls-instance',
+            'USER': 'admin',
+            'PASSWORD': 'drusiri123',
+            'NAME': 'main',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'main',
+            'USER': 'admin',
+            'PASSWORD': 'drusiri123',
+        }
+    }
+# [END db_setup]
 
 
 # Password validation
@@ -120,4 +152,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = 'mobileadmin-home'
-ALLOWED_HOSTS = ['chiguru-ecospace-website.el.r.appspot.com']
+ALLOWED_HOSTS = ['chiguru-ecospace-website.el.r.appspot.com', 'localhost']
